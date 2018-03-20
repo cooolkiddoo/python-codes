b0f9e92d064bf24a18b28ba9f21df27a2484e22b
cooolkiddoo <37336265+cooolkiddoo@users.noreply.github.com>
2018-03-20 09:55:12 +0530

    Update lar.py

--- beginner/lar.py
+++ beginner/lar.py
@@ -1,6 +1,6 @@
-num1 = 1
-num2 = 2
-num3 = 3
+num1 = float(input("enter a number"))
+num2 = float(input("enter a number"))
+num3 = float(input("enter a number"))
 if (num1 >= num2) and (num1 >= num3):
    largest = num1
 elif (num2 >= num1) and (num2 >= num3):
