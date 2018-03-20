0a0881c7b3be96f0815a09aad70a0ec744756b40
cooolkiddoo <37336265+cooolkiddoo@users.noreply.github.com>
2018-03-20 11:52:56 +0530

    Update leap yr.py

--- beginner/leap yr.py
+++ beginner/leap yr.py
@@ -1,9 +1,5 @@
-Year = int(input("Please Enter a Year to check it is leap Year or not : "))  
-if (Year % 400 == 0):  
-    print("%d is a leap Year."%Year)  
-elif (Year % 100 == 0):  
-    print("%d is not a leap Year."%Year)  
-elif (Year % 4 == 0):  
-    print("%d is a leap Year."%Year)  
-else:  
-    print("%d is not a leap Year."%Year)    
+x=int(input(" "))
+if(x%4==0):
+	print("the year is a leap yr")
+else:
+	print("not a leap yr")
