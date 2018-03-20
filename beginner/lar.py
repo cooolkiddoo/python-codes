40985d7378ba01ecf83d3c6b41061ea7aa6e9afa
cooolkiddoo <37336265+cooolkiddoo@users.noreply.github.com>
2018-03-20 10:04:18 +0530

    Update lar.py

--- beginner/lar.py
+++ beginner/lar.py
@@ -1,11 +1,12 @@
-num1 = float(input("enter a number"))
-num2 = float(input("enter a number"))
-num3 = float(input("enter a number"))
-if (num1 >= num2) and (num1 >= num3):
+num1 = 1
+num2 = 2
+num3 = 3
+ 
+if (num1 > num2) and (num1 > num3):
    largest = num1
-elif (num2 >= num1) and (num2 >= num3):
+elif (num2 > num1) and (num2 > num3):
    largest = num2
 else:
    largest = num3
-
-print("The largest number between",num1,",",num2,"and",num3,"is",largest)
+ 
+print("The largest number is",largest)
