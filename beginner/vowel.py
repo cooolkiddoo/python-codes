93e4181c8373b1d10fcd0e39e7f2b998893a6b3d
cooolkiddoo <37336265+cooolkiddoo@users.noreply.github.com>
2018-03-20 09:25:30 +0530

    Update vowel.py

--- beginner/vowel.py
+++ beginner/vowel.py
@@ -2,7 +2,7 @@ list=["a","e","i","o","u","A","E","I","O","U"]
 t=raw_input("")
 print(t)
 if (t in list):
-    print ("vowel")
+    print ("Vowel")
 else:
-    print ("consonant")
+    print ("Consonant")
  
