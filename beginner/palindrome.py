233e30c0bc8aa129c6158e14d2b21956c3995658
cooolkiddoo <37336265+cooolkiddoo@users.noreply.github.com>
2018-03-20 14:26:11 +0530

    Update palindrome.py

--- beginner/palindrome.py
+++ beginner/palindrome.py
@@ -1,10 +1,10 @@
-n=int(input("Enter number:"))
-temp=n
+num=int(input("Enter number:"))
+temp=num
 rev=0
-while(n>0):
+while(num>0):
     dig=n%10
     rev=rev*10+dig
-    n=n//10
+    num=num//10
 if(temp==rev):
     print("The number is a palindrome!")
 else:
